1ecb8ce6afecd77af7a3ac89ef6236f8aa436bfb
yangwenxu <yangwenxu>
2020-09-24 13:27:17 +0800

    预检请求

--- 01/api.js
+++ 01/api.js
@@ -12,12 +12,23 @@ let server = http.createServer((req, res) => {
 				res.end(data)
 			}
 		})
-	} else if (method == 'GET' && url =="/api/users") {
+	} else if ((method == 'GET' || method == 'POST' )&& url =="/api/users") {
 		res.setHeader("Content-Type", "application/json");
+		res.setHeader('Access-Control-Allow-Credentials', 'true');
+		res.setHeader('Set-Cookie', 'cookie1=va222;')
+		res.setHeader("Access-Control-Allow-Origin", 'http://localhost:3000')
 		res.end(JSON.stringify([{
 			name: 'zhaobuchu',
 			age: 22
 		}]))
+	} else if (method == "OPTIONS" && url == "/api/users") {
+		res.setHeader('Access-Control-Allow-Credentials', 'true');
+		res.writeHead(200, {
+			"Access-Control-Allow-Origin": "http://localhost:3000",
+			"Access-Control-Allow-Headers": "X-Token,Content-Type",
+			"Access-Control-Allow-Methods": "PUT"
+		});
+		res.end();
 	}
 })
 
--- 01/proxy.js
+++ 01/proxy.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
+// const proxy = require('http-proxy-middleware')
 
 app.use(express.static(__dirname + '/'))	
-
+// app.use('/api', proxy({ target: 'http://localhost:4000', changeOrigin: false
+// }));
 
 module.exports  = app
